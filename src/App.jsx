6ae763532980f197ba77a01bfa4667156a3cf41f
Carlos Calou <carloscalou6@gmail.com>
2026-01-20 14:42:48 -0300

    feat: create basic structure project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
 export default function App() {
-  return "Hello, React!";
+  return (
+    <header>
+      <a href="/">bikecraft</a>
+      <nav>
+        <ul>
+          <li>
+            <a href="/products">product</a>
+          </li>
+          <li>
+            <a href="/about">about</a>
+          </li>
+          <li>
+            <a href="/contact">contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
