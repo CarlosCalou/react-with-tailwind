93855a905ee45ee94a59168095c6b9aecbad297c
Carlos Calou <carloscalou6@gmail.com>
2026-01-20 16:36:42 -0300

    feat: create header bikcraft

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 export default function App() {
   return (
-    <header>
-      <a href="/">bikecraft</a>
-      <nav>
-        <ul>
+    <header className="bg-black text-amber-50 px-42 py-7 flex justify-between items-center">
+      <a
+        className="text-3xl tracking-wide font-semibold hover:text-amber-200"
+        href="/"
+      >
+        bikcraft
+      </a>
+      <nav className="">
+        <ul className="flex gap-8">
           <li>
-            <a href="/products">product</a>
+            <a
+              className="capitalize text-[1.1rem] hover:text-yellow-300 tracking-wide font-medium"
+              href="/products"
+            >
+              product
+            </a>
           </li>
           <li>
-            <a href="/about">about</a>
+            <a
+              className="capitalize text-[1.1rem] hover:text-yellow-300 tracking-wide font-medium "
+              href="/about"
+            >
+              about
+            </a>
           </li>
           <li>
-            <a href="/contact">contact</a>
+            <a
+              className="capitalize text-[1.1rem] hover:text-yellow-300 tracking-wide font-medium"
+              href="/contact"
+            >
+              contact
+            </a>
           </li>
         </ul>
       </nav>
